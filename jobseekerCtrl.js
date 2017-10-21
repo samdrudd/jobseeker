a92ad6fa30efb22ca9f4942ea319aee21a35f87c
Sam Rudd <samdrudd@gmail.com>
2017-10-21 16:04:04 -0700

    added table to show all jobs

--- jobseekerCtrl.js
+++ jobseekerCtrl.js
@@ -1,9 +1,24 @@
 app.controller("jobseekerCtrl", function($scope, $http) {
-	var today = new Date();
-	$scope.job = {};
-	$scope.job.date = (today.getMonth()+1) + "/" + today.getDate() + "/" + today.getFullYear();
+
+	$scope.init = function() {
+		$scope.job = {};
+		$scope.job.date = $scope.getDateString(new Date());
+		$scope.joblist = [];
+
+		$http({
+			url: "http://127.0.0.1:8000/jobs",
+			method: "GET",
+			headers: {"Content-Type": "application/x-www-form-urlencoded"} })
+		.then(function successCallback(response) {
+			console.log(response.data);
+			$scope.joblist = response.data;
+		}, function errorCallback(response) {
+			console.log(response.statusText);
+		});
+	};
 
 	$scope.addJob = function() {
+		console.log($scope.job.date);
 		$http({
 			url: "http://127.0.0.1:8000/jobs",
 			method: "POST",
@@ -11,14 +26,23 @@ app.controller("jobseekerCtrl", function($scope, $http) {
 			headers: { "Content-Type": "application/x-www-form-urlencoded"}})
 		.then(function successCallback(response) {
 			console.log(response.data);
+			$scope.joblist.push($scope.job);
 		}, function errorCallback(response) {
 			console.log(response.statusText);
 		});
+
+		$scope.resetForm();
 
 	};
 
 	$scope.resetForm = function() {
 		$scope.job = {};
+		$scope.job.date = $scope.getDateString(new Date());
+	};
+
+	$scope.getDateString = function(date) {
+		var dt = new Date(date);
+		return (dt.getMonth()+1) + "/" + dt.getDate() + "/" + dt.getFullYear();
 	};
 
 });
