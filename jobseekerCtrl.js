5f64a31c427d425d7927eadd7fb71267864b6974
Sam Rudd <samdrudd@gmail.com>
2017-10-24 02:57:40 -0700

    fix for issue #5 - being unable to delete newly added jobs

--- jobseekerCtrl.js
+++ jobseekerCtrl.js
@@ -16,28 +16,24 @@ app.controller("jobseekerCtrl", function($scope, $http) {
 	};
 
 	$scope.addJob = function() {
-		var jobobj = $scope.getJobObject($scope.job);
-
 		$http({
 			url: "http://127.0.0.1:8000/jobs",
 			method: "POST",
 			data: $.param($scope.job),
 			headers: { "Content-Type": "application/x-www-form-urlencoded"}})
 		.then(function successCallback(response) {
-			$scope.joblist.push(jobobj);
+			$scope.joblist.push(response.data);
 		}, function errorCallback(response) {
 			console.log(response.statusText);
 		});
 
 		$scope.resetForm();
-
 	};
 
 	$scope.deleteJob = function(id, index) {
 		$http({
 			url: "http://127.0.0.1:8000/jobs/" + id,
-			method: "DELETE",
-			headers: { "Content-Type" : "application/x-www-form-urlencoded" } })
+			method: "DELETE" })
 		.then(function(response) {
 			$scope.joblist.splice(index, 1);
 		}, function(response) {
@@ -54,10 +50,6 @@ app.controller("jobseekerCtrl", function($scope, $http) {
 	$scope.getDateString = function(date) {
 		var dt = new Date(date);
 		return (dt.getMonth()+1) + "/" + dt.getDate() + "/" + dt.getFullYear();
-	};
-
-	$scope.getJobObject = function(job) {
-		return { date : job.date, title : job.title, company : job.company, location : job.location, via : job.via };
 	};
 
 });
