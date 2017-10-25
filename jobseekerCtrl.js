89739c78282dccd563305bd9e40c076c8fb6f8c8
Sam Rudd <samdrudd@gmail.com>
2017-10-25 03:49:36 -0700

    making progress towards sorting functionality, some bits of form validation thrown in

--- jobseekerCtrl.js
+++ jobseekerCtrl.js
@@ -1,5 +1,6 @@
 app.controller("jobseekerCtrl", function($scope, $http) {
-
+	$scope.sorted = {};
+	
 	$scope.init = function() {
 		$scope.job = {};
 		$scope.job.date = $scope.getDateString(new Date());
@@ -16,6 +17,13 @@ app.controller("jobseekerCtrl", function($scope, $http) {
 	};
 
 	$scope.addJob = function() {
+		var form = document.getElementById("addJobForm");
+		if (!form.checkValidity())
+		{
+			form.classList.add("was-validated");
+			return;
+		}
+		console.log(form);
 		$http({
 			url: "http://127.0.0.1:8000/jobs",
 			method: "POST",
@@ -24,7 +32,7 @@ app.controller("jobseekerCtrl", function($scope, $http) {
 		.then(function successCallback(response) {
 			$scope.joblist.push(response.data);
 		}, function errorCallback(response) {
-			console.log(response.statusText);
+			console.log(response);
 		});
 
 		$scope.resetForm();
@@ -45,11 +53,34 @@ app.controller("jobseekerCtrl", function($scope, $http) {
 	$scope.resetForm = function() {
 		$scope.job = {};
 		$scope.job.date = $scope.getDateString(new Date());
+		document.getElementById("addJobForm").classList.remove("was-validated");
 	};
 
 	$scope.getDateString = function(date) {
 		var dt = new Date(date);
 		return (dt.getMonth()+1) + "/" + dt.getDate() + "/" + dt.getFullYear();
 	};
-
+	
+	$scope.sort = function(colname) {
+		console.log($scope.sorted);
+		if ($scope.sorted.column !== colname) {
+			$scope.sorted.column = colname;
+			$scope.sorted.order = "des";
+			$scope.joblist.sort(function(a, b) {
+					var x = a[colname].toLowerCase();
+					var y = b[colname].toLowerCase();
+					if (x < y) { return -1; }
+					if (x > y) { return 1; }
+					return 0;
+				});
+		} else {
+			$scope.joblist.reverse();
+			$scope.sorted.order === "asc" ? $scope.sorted.order = "des" : $scope.sorted.order = "asc";
+		}	
+	};
+	
+	$scope.isSortedBy = function(colname) {
+		return $scope.sorted.column === colname;
+	};
+	
 });
